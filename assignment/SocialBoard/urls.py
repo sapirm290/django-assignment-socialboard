b006777d141e6f89c325d3b4eea4fda09b6ae748
Sapir Malka <sapirm290@gmail.com>
2019-08-04 13:10:13 +0300

    Login and registration(A bit of a mess still, but works)

--- assignment/SocialBoard/urls.py
+++ assignment/SocialBoard/urls.py
@@ -1,6 +1,11 @@
 from . import views
 from django.urls import path
+# from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+
+
 ]
--- assignment/SocialBoard/views.py
+++ assignment/SocialBoard/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
+
+@login_required
 def index(request):
     posts = Post.objects.all()
-    return render(request, 'SocialBoard/posts.html', {'posts':posts})
+    return render(request, 'SocialBoard/posts.html', {'posts': posts})
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
+    template_name = 'signup.html'
